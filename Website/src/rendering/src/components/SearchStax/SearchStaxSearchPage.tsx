6d55e29cd35c13533416563e536b20eba6572488
Hugodelimasantos88 <hugolima88@gmail.com>
2022-09-16 16:02:14 -0300

    Add draft for SearchStax page in React

---
+++ Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
@@ -0,0 +1,7 @@
+  export default SearchStaxSearchPage;
+
+  const SearchStaxSearchPage = (): JSX.Element => (
+    <div>SearchStax Search Page goes here.</div>
+  );
+  
+  export default SearchStaxSearchPage;
