0bc0a8a857b0b14092bc4645009d610f119b923c
Hugodelimasantos88 <hugolima88@gmail.com>
2022-09-19 16:28:08 -0300

    revert2

--- Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
+++ Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
@@ -4,6 +4,7 @@ const SearchPage = (): JSX.Element => {
   return (
     <div>
       <Script src="https://static.searchstax.com/studio-js/v3/js/studio-app.js"></Script>
+      <Script src="https://static.searchstax.com/studio-js/v3/js/studio-app.js"></Script>
 
       <div>
         <div>SearchStax for Sitecore</div>
@@ -11,21 +12,21 @@ const SearchPage = (): JSX.Element => {
       </div>
 
       <div className="input-container">
-        {/* <div class="sf-header-searchstudio-js mb-5">
+        <div className="sf-header-searchstudio-js mb-5">
           <div>
-              <div class="sf-form">
-                <div class="form-group">
-                    <div id="autosuggest" class="form-control-suggest">
-                      <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results"><input type="text" autocomplete="off" aria-autocomplete="list" aria-activedescendant="" aria-controls="autosuggest-autosuggest__results" id="searchTerm" placeholder="Search For..." autofocus="autofocus" class=""></div>
-                      <div id="autosuggest-autosuggest__results" class="autosuggest__results-container">
+              <div className="sf-form">
+                <div className="form-group">
+                    <div id="autosuggest" className="form-control-suggest">
+                      <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results"><input type="text" autocomplete="off" aria-autocomplete="list" aria-activedescendant="" aria-controls="autosuggest-autosuggest__results" id="searchTerm" placeholder="Search For..." autofocus="autofocus" className=""></div>
+                      <div id="autosuggest-autosuggest__results" className="autosuggest__results-container">
 
                       </div>
                     </div>
-                    <span><button type="submit" class="btn text-primary search-close-button"><span class="search-icon"></span></button></span>
+                    <span><button type="submit" className="btn text-primary search-close-button"><span className="search-icon"></span></button></span>
                 </div>
               </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
   );
