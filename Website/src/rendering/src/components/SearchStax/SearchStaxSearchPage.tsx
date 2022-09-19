d6f510e0a46def40c45bf8e158fdba75c7f147e4
Hugodelimasantos88 <hugolima88@gmail.com>
2022-09-19 16:39:56 -0300

    test

--- Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
+++ Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
@@ -11,21 +11,21 @@ const SearchPage = (): JSX.Element => {
         <div>Symposium Demo!</div>
       </div>
 
-      {/* <div className="input-container">
+      <div className="input-container">
         <div className="sf-header-searchstudio-js mb-5">
           <div>
             <div className="sf-form">
               <div className="form-group">
-                <div id="autosuggest" className="form-control-suggest">
+                {/* <div id="autosuggest" className="form-control-suggest">
                   <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results"><input type="text" autocomplete="off" aria-autocomplete="list" aria-activedescendant="" aria-controls="autosuggest-autosuggest__results" id="searchTerm" placeholder="Search For..." autofocus="autofocus" className=""></input></div>
                   <div id="autosuggest-autosuggest__results" className="autosuggest__results-container"></div>
                 </div>
-                <span><button type="submit" className="btn text-primary search-close-button"><span className="search-icon"></span></button></span>
+                <span><button type="submit" className="btn text-primary search-close-button"><span className="search-icon"></span></button></span> */}
               </div>
             </div>
           </div>
         </div>
-      </div> */}
+      </div>
     </div>
   );
 };
