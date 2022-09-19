88440222be5acedc0879b66ef6b1e6dc05bb4175
Hugodelimasantos88 <hugolima88@gmail.com>
2022-09-19 16:05:00 -0300

    test

--- Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
+++ Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
@@ -11,16 +11,16 @@ const SearchPage = (): JSX.Element => {
         <div>Symposium Demo!</div>
       </div>
 
-      <div className="input-container">
+      {/* <div className="input-container">
         <div className="sf-header-searchstudio-js mb-5">
           <div>
             <div className="sf-form">
               <div className="form-group">
                 <div id="autosuggest" className="form-control-suggest">
-                  {/* <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results">
+                  <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results">
                     <input type="text" autocomplete="off" aria-autocomplete="list" aria-activedescendant="" aria-controls="autosuggest-autosuggest__results" id="searchTerm" placeholder="Search For..." autofocus="autofocus" className=""></input>
                   </div>
-                  <div id="autosuggest-autosuggest__results" className="autosuggest__results-container"></div> */}
+                  <div id="autosuggest-autosuggest__results" className="autosuggest__results-container"></div>
                 </div>
                 <span>
                   <button type="submit" className="btn text-primary search-close-button">
@@ -31,7 +31,7 @@ const SearchPage = (): JSX.Element => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
