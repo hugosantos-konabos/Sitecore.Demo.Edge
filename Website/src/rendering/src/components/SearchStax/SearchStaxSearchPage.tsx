482f56a4bb54ce13a5fd6b328e92b4ddc367874a
Hugodelimasantos88 <hugolima88@gmail.com>
2022-09-19 16:50:39 -0300

    test

--- Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
+++ Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
@@ -16,11 +16,35 @@ const SearchPage = (): JSX.Element => {
           <div>
             <div className="sf-form">
               <div className="form-group">
-                {/* <div id="autosuggest" className="form-control-suggest">
-                  <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results"><input type="text" autocomplete="off" aria-autocomplete="list" aria-activedescendant="" aria-controls="autosuggest-autosuggest__results" id="searchTerm" placeholder="Search For..." autofocus="autofocus" className=""></input></div>
-                  <div id="autosuggest-autosuggest__results" className="autosuggest__results-container"></div>
+                <div id="autosuggest" className="form-control-suggest">
+                  <div
+                    role="combobox"
+                    aria-expanded="false"
+                    aria-haspopup="listbox"
+                    aria-owns="autosuggest-autosuggest__results"
+                  >
+                    <input
+                      type="text"
+                      autoComplete="off"
+                      aria-autocomplete="list"
+                      aria-activedescendant=""
+                      aria-controls="autosuggest-autosuggest__results"
+                      id="searchTerm"
+                      placeholder="Search For..."
+                      // autoFocus="autofocus"
+                      className=""
+                    ></input>
+                  </div>
+                  <div
+                    id="autosuggest-autosuggest__results"
+                    className="autosuggest__results-container"
+                  ></div>
                 </div>
-                <span><button type="submit" className="btn text-primary search-close-button"><span className="search-icon"></span></button></span> */}
+                <span>
+                  <button type="submit" className="btn text-primary search-close-button">
+                    <span className="search-icon"></span>
+                  </button>
+                </span>
               </div>
             </div>
           </div>
