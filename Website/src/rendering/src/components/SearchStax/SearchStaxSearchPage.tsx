ad4c4cd4a62cdcb9257ab6728b0efbc131ca9e01
Hugodelimasantos88 <hugolima88@gmail.com>
2022-09-19 15:33:51 -0300

    test

--- Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
+++ Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
@@ -4,7 +4,7 @@ const SearchPage = (): JSX.Element => {
   return (
     <div>
       <Script src="https://static.searchstax.com/studio-js/v3/js/studio-app.js"></Script>
-      <Script src="https://static.searchstax.com/studio-js/v3/js/studio-app.js"></Script>
+      <Script src="https://static.searchstax.com/studio-js/v3/js/studio-vendors.js"></Script>
 
       <div>
         <div>SearchStax for Sitecore</div>
@@ -14,17 +14,15 @@ const SearchPage = (): JSX.Element => {
       <div className="input-container">
         <div className="sf-header-searchstudio-js mb-5">
           <div>
-              <div className="sf-form">
-                <div className="form-group">
-                    <div id="autosuggest" className="form-control-suggest">
-                      <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results"><input type="text" autocomplete="off" aria-autocomplete="list" aria-activedescendant="" aria-controls="autosuggest-autosuggest__results" id="searchTerm" placeholder="Search For..." autofocus="autofocus" className=""></div>
-                      <div id="autosuggest-autosuggest__results" className="autosuggest__results-container">
-
-                      </div>
-                    </div>
-                    <span><button type="submit" className="btn text-primary search-close-button"><span className="search-icon"></span></button></span>
+            <div className="sf-form">
+              <div className="form-group">
+                <div id="autosuggest" className="form-control-suggest">
+                  <div role="combobox" aria-expanded="false" aria-haspopup="listbox" aria-owns="autosuggest-autosuggest__results"><input type="text" autocomplete="off" aria-autocomplete="list" aria-activedescendant="" aria-controls="autosuggest-autosuggest__results" id="searchTerm" placeholder="Search For..." autofocus="autofocus" className=""></input></div>
+                  <div id="autosuggest-autosuggest__results" className="autosuggest__results-container"></div>
                 </div>
+                <span><button type="submit" className="btn text-primary search-close-button"><span className="search-icon"></span></button></span>
               </div>
+            </div>
           </div>
         </div>
       </div>
