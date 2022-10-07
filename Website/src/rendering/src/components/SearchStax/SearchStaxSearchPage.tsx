40a9277231b68b2b60e55f0f601fd96aeb5143c2
Hugodelimasantos88 <hugolima88@gmail.com>
2022-10-07 13:13:19 -0300

    test

--- Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
+++ Website/src/rendering/src/components/SearchStax/SearchStaxSearchPage.tsx
@@ -68,7 +68,7 @@ const SearchPage = (props: SearchStaxSearchPageProps): JSX.Element => {
         })();`}</Script>
 
       <div className="app">
-        <div id="test">${props.fields.SearchStaxIndex}</div>
+        <div id="test">${props.fields.SearchStaxIndex.name}</div>
         <div className="search-input-wrapper">
           <div className="feedback-wrapper">
             <div id="sf-feedback"></div>
--- Website/src/rendering/src/types/searchStaxIndex.ts
+++ Website/src/rendering/src/types/searchStaxIndex.ts
@@ -1,17 +1,18 @@
 import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
 
 export type SearchStaxIndex = {
-  fields: {
-    SearchModelDefinition: Field<string>;
-    OverrideIndexCoreName: Field<string>;
-    SearchModel: Field<string>;
-    SearchEndpointUrl: Field<string>;
-    ApiKey: Field<string>;
-    FieldsMapping: Field<string>;
-    SuggesterEndpointUrl: Field<string>;
-    SearchApiKey: Field<string>;
-    AnalyticsApiKey: Field<string>;
-    SolrUsername: Field<string>;
-    SolrPassword: Field<string>;
-  };
+  name: Field<string>;
+  // fields: {
+  //   SearchModelDefinition: Field<string>;
+  //   OverrideIndexCoreName: Field<string>;
+  //   SearchModel: Field<string>;
+  //   SearchEndpointUrl: Field<string>;
+  //   ApiKey: Field<string>;
+  //   FieldsMapping: Field<string>;
+  //   SuggesterEndpointUrl: Field<string>;
+  //   SearchApiKey: Field<string>;
+  //   AnalyticsApiKey: Field<string>;
+  //   SolrUsername: Field<string>;
+  //   SolrPassword: Field<string>;
+  // };
 };
